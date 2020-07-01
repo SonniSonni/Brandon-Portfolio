132eec920e727b770849f9d03d1b3cef95778f1d
KamajiiSC <brandonlschaen@gmail.com>
2020-07-01 13:46:50 -0400

    Created component for Header, updated some dependencies, and Set up the dev-server into webpack config

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+const Header = () => (
+  <header>
+    <img src="../resources/portfolio-logo.svg" alt="" />
+    <div id="header-spacer"></div>
+    <nav>
+          <ul>
+            <li><a href="#"> <h2>Home</h2> </a></li>
+            <li><a href="#"> <h2>Portfolio</h2> </a></li>
+            <li><a href="#"> <h2>About</h2> </a></li>
+            <li><a href="#"> <h2>Contact</h2> </a></li>
+          </ul>
+    </nav>
+  </header>
+);
