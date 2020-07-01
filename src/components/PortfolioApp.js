b045253fe56b28a74249c540f9d8a5f8fdcc4e2d
KamajiiSC <brandonlschaen@gmail.com>
2020-07-01 14:02:04 -0400

    Created default class for PortfolioApp and render Header

--- src/components/PortfolioApp.js
+++ src/components/PortfolioApp.js
@@ -1,3 +1,13 @@
 import React from 'react';
 import Header from './Header';
 import Hero from './Hero';
+
+export default class PortfolioApp extends React.Component{
+  render(){
+    return(
+      <div>
+        <Header />
+      </div>
+    );
+  };
+};
