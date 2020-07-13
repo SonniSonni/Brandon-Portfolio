a32c72ae4a96f865fe4dd142224ab3f1662bc66e
KamajiiSC <brandonlschaen@gmail.com>
2020-07-13 16:02:02 -0400

    Set empty src and alts for the two imgs

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const GalleryItem = () => (
   <div>
-    <img />
+    <img src={} alt="Preview of website"/>
     <h3></h3>
-    <img />
+    <img src={} alt="git logo"/>
     <p></p>
   </div>
 );
