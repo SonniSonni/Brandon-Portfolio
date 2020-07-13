3adc738bdca2050e9437a5fac229efb96b10eecf
KamajiiSC <brandonlschaen@gmail.com>
2020-07-13 16:04:03 -0400

    Imported galleryitem and added a ul with 3 items in GalleryPrev

--- src/components/GalleryPrev.js
+++ src/components/GalleryPrev.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import GalleryItem from './GalleryItem';
 
 const GalleryPrev = () => (
   <div>
     <h2>Gallery</h2>
     <hr />
+    <ul>
+      <li> </li>
+      <li> </li>
+      <li> </li>
+    </ul>
     <hr />
   </div>
 );
