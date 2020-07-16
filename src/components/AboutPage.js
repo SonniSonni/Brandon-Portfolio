4d600e4a26b71f2dd611e634db2b7f264912e292
unknown <brandonlschaen@gmail.com>
2020-07-16 13:22:43 -0400

    Added Header to the AboutPage

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './Header.js';
 
 export default class AboutPage extends React.Component {
     render(){
         return(
             <div>
-                
+                <Header />
             </div>
         )
     }
