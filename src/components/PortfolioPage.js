c811cba7baf06a4f630fe3732fa081c2d8fe70f2
unknown <brandonlschaen@gmail.com>
2020-07-16 13:20:50 -0400

    Added footer to the Portfolio Page

--- src/components/PortfolioPage.js
+++ src/components/PortfolioPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header.js';
+import Footer from './Footer.js';
 
 export default class PortfolioPage extends React.Component{
     render(){
         return(
             <div>
                 <Header />
+                <Footer />
             </div>
         )
     }
