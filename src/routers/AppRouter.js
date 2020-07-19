828e877c582dbaeca0b7372bda20c0762302b13f
KamajiiSC <brandonlschaen@gmail.com>
2020-07-19 19:46:22 -0400

    Imported things from react-router-dom and created an instance of BrowserRouter

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 
 const AppRouter = () => (
-  <div>
-    
-  </div>
+  <BrowserRouter>
+    <div>
+
+    </div>
+  </BrowserRouter>
 );
