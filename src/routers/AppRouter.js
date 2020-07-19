fe9ae91330ea34cd48f40754993a114560280281
KamajiiSC <brandonlschaen@gmail.com>
2020-07-19 19:49:37 -0400

    Imported two more pages in AppRouter

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
-import HomePage from '../components/HomePage'
+import HomePage from '../components/HomePage';
+import PortfolioPage from '../components/PortfolioPage';
+import AboutPage from '../components/AboutPage';
 
 const AppRouter = () => (
   <BrowserRouter>
     <div>
       <Switch>
+        <Route path='/' component={HomePage}></Route>
+        <Route path='/' component={HomePage}></Route>
+        <Route path='/' component={HomePage}></Route>
         <Route path='/' component={HomePage}></Route>
       </Switch>
     </div>
