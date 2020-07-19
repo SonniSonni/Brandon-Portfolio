4a003b8e8c01171c39c4edca6afd9459d8a722f7
KamajiiSC <brandonlschaen@gmail.com>
2020-07-19 18:43:57 -0400

    Set apiFallback and publicPath in webpack

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,9 +7,9 @@ const Header = () => (
     <div id="header-spacer"></div>
     <nav>
       <ul>
-        <li><a href="#"> <h2>Home</h2> </a></li>
-        <li><a href="#"> <h2>Portfolio</h2> </a></li>
-        <li><a href="#"> <h2>About</h2> </a></li>
+        <li><a href="./HomePage"> <h2>Home</h2> </a></li>
+        <li><a href="./PortfolioPage"> <h2>Portfolio</h2> </a></li>
+        <li><a href="./AboutPage"> <h2>About</h2> </a></li>
         <li><a href="#"> <h2>Contact</h2> </a></li>
       </ul>
     </nav>
--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,8 @@ module.exports = {
   },
   devtool: 'cheap-module-eval-source-map',
   devServer: {
-    contentBase:  path.join(__dirname, 'public')
+    contentBase:  path.join(__dirname, 'public'),
+    historyApiFallback: true,
+    publicPath: '/dist/'
   }
 };
