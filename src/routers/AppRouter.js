6d98204a92f90e0736f1a891a4f56320baca46d2
KamajiiSC <brandonlschaen@gmail.com>
2020-07-19 19:55:27 -0400

    Put Header inside of AppRouter

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import Header from '../components/Header';
 import HomePage from '../components/HomePage';
 import PortfolioPage from '../components/PortfolioPage';
 import AboutPage from '../components/AboutPage';
@@ -8,6 +9,7 @@ import ContactPage from '../components/ContactPage';
 const AppRouter = () => (
   <BrowserRouter>
     <div>
+      <Header />
       <Switch>
         <Route path='/' component={HomePage}></Route>
         <Route path='/PortfolioPage' component={PortfolioPage}></Route>
