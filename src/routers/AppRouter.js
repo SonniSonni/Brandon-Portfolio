fade4b34a23df82a01b27db7d9f84e7c57c64890
KamajiiSC <brandonlschaen@gmail.com>
2020-07-19 19:48:23 -0400

    Created a switch and imported HomePage for first route

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import HomePage from '../components/HomePage'
 
 const AppRouter = () => (
   <BrowserRouter>
     <div>
-
+      <Switch>
+        <Route path='/' component={HomePage}></Route>
+      </Switch>
     </div>
   </BrowserRouter>
 );
