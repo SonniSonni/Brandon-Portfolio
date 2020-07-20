f699617d6c81fa1235dabdc5d3bcc1066c8098af
KamajiiSC <brandonlschaen@gmail.com>
2020-07-20 15:56:13 -0400

    Put EmailBox and MessageBox inside contact container

--- src/components/ContactContainer.js
+++ src/components/ContactContainer.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import EmailBox from './EmailBox';
+import MessageBox from './MessageBox';
 
 const ContactContainer = () => (
   <div>
     <h1>Contact Me</h1>
+    <EmailBox />
+    <MessageBox />
   </div>
 );
 
