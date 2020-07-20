576f47b17a902f8414ae240353c51f452185abd4
KamajiiSC <brandonlschaen@gmail.com>
2020-07-19 20:08:30 -0400

    Setup class component for ContactPage

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Header from './Header.js';
 import Footer from './Footer.js';
 
 export default class AboutPage extends React.Component {
--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -1,1 +1,11 @@
 import React from 'react';
+
+export default class ContactPage extends React.Component {
+  render(){
+    return(
+      <div>
+        
+      </div>
+    )
+  }
+}
