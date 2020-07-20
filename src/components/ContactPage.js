ea810b43ab5ddd744f7bf19aab2e7063056925d4
KamajiiSC <brandonlschaen@gmail.com>
2020-07-20 15:19:25 -0400

    Inserted ContactContainer inside of ContactPage

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Footer from './Footer';
+import ContactContainer from './ContactContainer'
 
 export default class ContactPage extends React.Component {
   render(){
     return(
       <div>
+        <ContactContainer />
         <Footer />
       </div>
     )
