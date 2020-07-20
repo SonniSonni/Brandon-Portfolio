b7cdf95706b18f45155130c22f7fef1e7c8d3a03
KamajiiSC <brandonlschaen@gmail.com>
2020-07-20 15:15:01 -0400

    Put AboutContainer inside of AboutPage and Created h1 title for abvout container

--- src/components/AboutContainer.js
+++ src/components/AboutContainer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AboutContainer = () => (
   <div>
-
+    <h1>About Me</h1>
   </div>
 );
 
--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Footer from './Footer.js';
+import AboutContainer from './AboutContainer.js';
 
 export default class AboutPage extends React.Component {
     render(){
         return(
             <div>
+                <AboutContainer />
                 <Footer />
             </div>
         )
