541b32c28e049817e07a98ee6450759047370f5f
KamajiiSC <brandonlschaen@gmail.com>
2020-07-21 14:26:08 -0400

    Created label for the button on About

--- src/components/AboutContainer.js
+++ src/components/AboutContainer.js
@@ -7,7 +7,8 @@ const AboutContainer = () => (
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tortor lorem, tempus vitae lorem at, lacinia cursus massa. Nulla facilisi. Vivamus accumsan mi non porttitor tempor. In egestas eu purus vel gravida. Aenean bibendum, sem nec auctor convallis, est ligula aliquam risus, in convallis ligula enim ut metus. Nam eu tellus rhoncus, eleifend risus vel, ullamcorper nunc. Phasellus nec turpis sit amet lacus accumsan vehicula. Quisque pharetra aliquam arcu nec imperdiet.
     </p>
     <img />
-    <button>See more</button>
+    <label>See my resume here: </label> <button>See more</button>
+   
   </div>
 );
 
