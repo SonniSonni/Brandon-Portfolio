3c8eeb436e4c40953db4b4fccd96c61c739fbd71
KamajiiSC <brandonlschaen@gmail.com>
2020-07-21 14:35:07 -0400

    Removed PortfolioItem and Imported GalleryItem into the PortfolioContainer

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import GalleryItem from './GalleryItem';
 
 const PortfolioContainer = () => (
     <div>
         <h1>My Work</h1>
+        <GalleryItem />
     </div>
 );
 
