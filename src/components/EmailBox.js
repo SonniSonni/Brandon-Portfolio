9311cfa2dcfcf7d7f5d2f2a108f72ceb77400e25
KamajiiSC <brandonlschaen@gmail.com>
2020-07-21 14:28:50 -0400

    Added H4 paragraph and img link to EmailBox

--- src/components/EmailBox.js
+++ src/components/EmailBox.js
@@ -3,6 +3,9 @@ import React from 'react';
 const EmailBox = () => (
   <div>
     <h2>Email Me</h2>
+    <h4>BrandonLSchaen@gmail.com</h4>
+    <p>Always looking for new opportunities!</p>
+    <a><img /></a>
   </div>
 );
 
