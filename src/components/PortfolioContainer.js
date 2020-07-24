e81b4d2880dad898602fcb812cca3e6030e5acef
KamajiiSC <brandonlschaen@gmail.com>
2020-07-24 16:15:17 -0400

    Made PortfolioContainer class based

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import GalleryItem from './GalleryItem';
 
-const PortfolioContainer = () => (
-    <div>
-        <h1>My Work</h1>
-        <GalleryItem />
-        <GalleryItem />
-        <GalleryItem />
-        <GalleryItem />
-        <GalleryItem />
-        <GalleryItem />
-    </div>
-);
+export default class PortfolioContainer extends React.Component{
+    render(){
+        return(
+        <div>
+            <h1>My Work</h1>
+            <GalleryItem />
+            <GalleryItem />
+            <GalleryItem />
+            <GalleryItem />
+            <GalleryItem />
+            <GalleryItem />
+        </div>
+    )}
+};
 
 export default PortfolioContainer;
