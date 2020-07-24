9c15fd4e9f8b385dd7355fa3126c883db3bda490
KamajiiSC <brandonlschaen@gmail.com>
2020-07-23 21:52:03 -0400

    Made GalleryItem class based

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const GalleryItem = (props) => (
-  <div>
-    <img src={'#'} alt="Preview of website"/>
-    <h3> </h3>
-    <img src={"#"} alt="git logo"/>
-    <p>  </p>
-  </div>
-);
-
-export default GalleryItem;
+export default class GalleryItem extends React.Component {
+  render(){
+    return (
+      <div>
+        <img src={'#'} alt="Preview of website"/>
+        <h3> </h3>
+        <img src={"#"} alt="git logo"/>
+        <p>  </p>
+      </div>
+    )
+  }
+};
