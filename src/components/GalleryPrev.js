d84366d5c6b5b9e929b337c51db8140558a8475a
KamajiiSC <brandonlschaen@gmail.com>
2020-07-25 14:06:22 -0400

    imported coffeeExpress and set state for it the first project

--- src/components/GalleryPrev.js
+++ src/components/GalleryPrev.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import GalleryItem from './GalleryItem';
+import CoffeeExpress from '../../resources/CoffeeExpress.png';
 
 export default class GalleryPrev extends React.Component {
  constructor(props) {
    super (props);
+   this.state = {
+    coffeeExpress: {id: 0, title: 'Coffee Express', imgSrc: CoffeeExpress, description: 'A mock up website for a coffee shop'}
+   }
  }
  render() {
    return(
