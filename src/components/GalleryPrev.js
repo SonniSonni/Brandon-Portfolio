56fc1bd478ab97f40f978d4448e9212a646db425
KamajiiSC <brandonlschaen@gmail.com>
2020-07-25 14:02:26 -0400

    Made GalleryPrev a class based component

--- src/components/GalleryPrev.js
+++ src/components/GalleryPrev.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import GalleryItem from './GalleryItem';
 
-const GalleryPrev = () => (
-  <div>
-    <h2>Gallery</h2>
-    <hr />
-    <ul>
-      <li> <GalleryItem /> </li>
-      <li> <GalleryItem /> </li>
-      <li> <GalleryItem /> </li>
-    </ul>
-    <hr />
-  </div>
-);
-
-export default GalleryPrev;
+export default class GalleryPrev extends React.Component {
+ render() {
+   return(
+    <div>
+      <h2>Gallery</h2>
+      <hr />
+      <ul>
+        <li> <GalleryItem /> </li>
+        <li> <GalleryItem /> </li>
+        <li> <GalleryItem /> </li>
+      </ul>
+      <hr />
+    </div>
+   )
+ }
+};
