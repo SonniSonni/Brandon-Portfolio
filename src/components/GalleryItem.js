9eb6d3a4ffa60b077c35cc9044811908efdbecc2
KamajiiSC <brandonlschaen@gmail.com>
2020-07-27 19:56:41 -0400

    Put description in GalleryItem before github logo

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -5,8 +5,8 @@ const GalleryItem = (props) => (
       <div id="gallery-item">
         <img src={props.imgSrc} alt="Preview of website" id="proj-prev"/>
         <h3> {props.title} </h3>
-        <img src={Github} alt="git logo" id="github-logo"/>
         <p> {props.description} </p>
+        <img src={Github} alt="git logo" id="github-logo"/>      
       </div>
 );
 
