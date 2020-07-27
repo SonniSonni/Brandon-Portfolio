e5b4e5a2093efb0d7b331f1f6e240d93e23cd8a1
KamajiiSC <brandonlschaen@gmail.com>
2020-07-27 15:59:54 -0400

    Gave both img files seperate id and gave each one a width percent

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -3,9 +3,9 @@ import Github from '../../resources/github-logo.png'
 
 const GalleryItem = (props) => (
       <div id="gallery-item">
-        <img src={props.imgSrc} alt="Preview of website"/>
+        <img src={props.imgSrc} alt="Preview of website" id="proj-prev"/>
         <h3> {props.title} </h3>
-        <img src={Github} alt="git logo"/>
+        <img src={Github} alt="git logo" id="github-logo"/>
         <p> {props.description} </p>
       </div>
 );
