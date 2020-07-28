7a8e4abc7e342149d5945b17ff5c48151c7bb1ba
KamajiiSC <brandonlschaen@gmail.com>
2020-07-27 20:16:44 -0400

    put github logo back, made it smaller, set distances again

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -6,8 +6,8 @@ const GalleryItem = (props) => (
         <img src={props.imgSrc} alt="Preview of website" id="proj-prev"/>
         <div id='flex-container'>
           <h3> {props.title} </h3>
-          <p> {props.description} </p>
-          <img src={Github} alt="git logo" id="github-logo"/>     
+          <img src={Github} alt="git logo" id="github-logo"/>    
+          <p> {props.description} </p>          
         </div> 
       </div>
 );
