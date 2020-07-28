6f9689a07bfacf0e8030ab8e23ab26401376ac25
KamajiiSC <brandonlschaen@gmail.com>
2020-07-28 16:55:06 -0400

    Created hero-img partial

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,8 @@ import heroImage from '../../resources/Hero.png'
 const Hero = () => (
       <img src={heroImage} 
       alt="Man standing infront city scape"
-      id='hero-img' />
+      id='hero-img' 
+      />
 );
 
 export default Hero;
