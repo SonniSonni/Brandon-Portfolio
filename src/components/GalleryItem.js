f3c07c03f13e588dbede1070ad0ddbe06e5050c4
KamajiiSC <brandonlschaen@gmail.com>
2020-07-27 20:10:37 -0400

    Wrapped subtext items in gallery item in flex container, and set them to flex with center align

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -4,9 +4,11 @@ import Github from '../../resources/github-logo.png'
 const GalleryItem = (props) => (
       <div id="gallery-item">
         <img src={props.imgSrc} alt="Preview of website" id="proj-prev"/>
-        <h3> {props.title} </h3>
-        <p> {props.description} </p>
-        <img src={Github} alt="git logo" id="github-logo"/>      
+        <div id='flex-container'>
+          <h3> {props.title} </h3>
+          <p> {props.description} </p>
+          <img src={Github} alt="git logo" id="github-logo"/>     
+        </div> 
       </div>
 );
 
