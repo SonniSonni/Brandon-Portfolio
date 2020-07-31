d2445b08a9b20334542904f58b366d999a382a8f
KamajiiSC <brandonlschaen@gmail.com>
2020-07-31 14:12:32 -0400

    Created about-container partial and imported it to styles

--- src/components/AboutContainer.js
+++ src/components/AboutContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const AboutContainer = () => (
-  <div>
+  <div id='about-container'>
     <h1>About Me</h1>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tortor lorem, tempus vitae lorem at, lacinia cursus massa. Nulla facilisi. Vivamus accumsan mi non porttitor tempor. In egestas eu purus vel gravida. Aenean bibendum, sem nec auctor convallis, est ligula aliquam risus, in convallis ligula enim ut metus. Nam eu tellus rhoncus, eleifend risus vel, ullamcorper nunc. Phasellus nec turpis sit amet lacus accumsan vehicula. Quisque pharetra aliquam arcu nec imperdiet.
