3cb47b7dd71e2652e837a7d094dfb73b83ea5544
KamajiiSC <brandonlschaen@gmail.com>
2020-07-31 15:01:55 -0400

    Imported an img to about container and floated it

--- src/components/AboutContainer.js
+++ src/components/AboutContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import portrait from '../../resources/test-headshot.jpg';
 
 const AboutContainer = () => (
   <div id='about-container'>
@@ -6,9 +7,9 @@ const AboutContainer = () => (
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tortor lorem, tempus vitae lorem at, lacinia cursus massa. Nulla facilisi. Vivamus accumsan mi non porttitor tempor. In egestas eu purus vel gravida. Aenean bibendum, sem nec auctor convallis, est ligula aliquam risus, in convallis ligula enim ut metus. Nam eu tellus rhoncus, eleifend risus vel, ullamcorper nunc. Phasellus nec turpis sit amet lacus accumsan vehicula. Quisque pharetra aliquam arcu nec imperdiet.
     </p>
-    <img src="#" alt="Portrait of man"/>
+    
     <label>See my resume here: </label> <button>See more</button>
-   
+    <img src={portrait}alt="Portrait of man"/>
   </div>
 );
 
