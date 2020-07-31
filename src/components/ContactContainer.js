22f5f0a33f18f1054d3015afcac43ef1167121a3
KamajiiSC <brandonlschaen@gmail.com>
2020-07-31 15:31:57 -0400

    Set id for contact container and created partial

--- src/components/ContactContainer.js
+++ src/components/ContactContainer.js
@@ -3,7 +3,7 @@ import EmailBox from './EmailBox';
 import MessageBox from './MessageBox';
 
 const ContactContainer = () => (
-  <div>
+  <div id="contact-container">
     <h1>Contact Me</h1>
     <EmailBox />
     <MessageBox />
