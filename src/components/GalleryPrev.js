eead65afa731f6a585df0be3d5a2508770900337
KamajiiSC <brandonlschaen@gmail.com>
2020-07-31 16:27:40 -0400

    Figured out githubLink, works properly now

--- src/components/GalleryPrev.js
+++ src/components/GalleryPrev.js
@@ -6,7 +6,7 @@ export default class GalleryPrev extends React.Component {
  constructor(props) {
    super (props);
    this.state = {
-    coffeeExpress: {id: 0, title: 'Coffee Express', imgSrc: CoffeeExpress, description: 'A mock up website for a coffee shop'}
+    coffeeExpress: {id: 0, title: 'Coffee Express', imgSrc: CoffeeExpress, description: 'A mock up website for a coffee shop', githubLink:'https://github.com/KamajiiSC/CoffeeExpress'}
    }
  }
  render() {
@@ -15,9 +15,9 @@ export default class GalleryPrev extends React.Component {
       <h1>Gallery</h1>
       <hr />
       <ul>
-        <li> <GalleryItem title={this.state.coffeeExpress.title} imgSrc={this.state.coffeeExpress.imgSrc} description={this.state.coffeeExpress.description}/> </li>
-        <li> <GalleryItem title={this.state.coffeeExpress.title} imgSrc={this.state.coffeeExpress.imgSrc} description={this.state.coffeeExpress.description}/> </li>
-        <li> <GalleryItem title={this.state.coffeeExpress.title} imgSrc={this.state.coffeeExpress.imgSrc} description={this.state.coffeeExpress.description}/> </li>
+        <li> <GalleryItem title={this.state.coffeeExpress.title} imgSrc={this.state.coffeeExpress.imgSrc} description={this.state.coffeeExpress.description} githubLink={this.state.coffeeExpress.githubLink}/> </li>
+        <li> <GalleryItem title={this.state.coffeeExpress.title} imgSrc={this.state.coffeeExpress.imgSrc} description={this.state.coffeeExpress.description} githubLink={this.state.coffeeExpress.githubLink}/> </li>
+        <li> <GalleryItem title={this.state.coffeeExpress.title} imgSrc={this.state.coffeeExpress.imgSrc} description={this.state.coffeeExpress.description} githubLink={this.state.coffeeExpress.githubLink}/> </li>
       </ul>
       <hr />
       <button>See More</button>
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -6,7 +6,7 @@ export default class PortfolioContainer extends React.Component{
     constructor(props) {
         super(props);
         this.state= {
-         coffeeExpress: {id: 0, title: 'Coffee Express', imgSrc: CoffeeExpress, description: 'A mock up website for a coffee shop', githubLink: 'https://github.com/KamajiiSC/CoffeeExpress'}      
+         coffeeExpress: {id: 0, title: 'Coffee Express', imgSrc: CoffeeExpress, description: 'A mock up website for a coffee shop', githubLink:'https://github.com/KamajiiSC/CoffeeExpress'}      
         };
     }
     render(){
@@ -14,7 +14,8 @@ export default class PortfolioContainer extends React.Component{
         <div id="portfolio-container" class="box-container">
             <h1>My Work</h1>
             <ul>
-                <li><GalleryItem title={this.state.coffeeExpress.title} imgSrc={this.state.coffeeExpress.imgSrc} description={this.state.coffeeExpress.description} githubLink={this.state.coffeeExpress.githubLink}/></li>
+                <li><GalleryItem title={this.state.coffeeExpress.title} imgSrc={this.state.coffeeExpress.imgSrc} description={this.state.coffeeExpress.description}
+                githubLink={this.state.coffeeExpress.githubLink}/></li>
                 <li><GalleryItem title={this.state.coffeeExpress.title} imgSrc={this.state.coffeeExpress.imgSrc} description={this.state.coffeeExpress.description} githubLink={this.state.coffeeExpress.githubLink}/></li>
                 <li><GalleryItem title={this.state.coffeeExpress.title} imgSrc={this.state.coffeeExpress.imgSrc} description={this.state.coffeeExpress.description} githubLink={this.state.coffeeExpress.githubLink}/></li>
                 <li><GalleryItem title={this.state.coffeeExpress.title} imgSrc={this.state.coffeeExpress.imgSrc} description={this.state.coffeeExpress.description} githubLink={this.state.coffeeExpress.githubLink}/></li>
