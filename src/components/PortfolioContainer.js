328d062a4551ca62ecdc94b8a552fab46dbb606f
KamajiiSC <brandonlschaen@gmail.com>
2020-08-04 17:47:43 -0400

    Fixed class to className in portfolioContainer. Defined coffeeExpress outside of class, removed state, and used spread operator to pass the objects properties into each gallery item component

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -2,25 +2,27 @@ import React from 'react';
 import GalleryItem from './GalleryItem';
 import CoffeeExpress from '../../resources/CoffeeExpress.png';
 
+const coffeeExpress = 
+{
+    id: 0, 
+    title: 'Coffee Express', 
+    imgSrc: CoffeeExpress, 
+    description: 'A mock up website for a coffee shop', 
+    githubLink:'https://github.com/KamajiiSC/CoffeeExpress'
+}
+
 export default class PortfolioContainer extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state= {
-         coffeeExpress: {id: 0, title: 'Coffee Express', imgSrc: CoffeeExpress, description: 'A mock up website for a coffee shop', githubLink:'https://github.com/KamajiiSC/CoffeeExpress'}      
-        };
-    }
     render(){
         return(
-        <div id="portfolio-container" class="box-container">
+        <div id="portfolio-container" className="box-container">
             <h1>My Work</h1>
             <ul>
-                <li><GalleryItem title={this.state.coffeeExpress.title} imgSrc={this.state.coffeeExpress.imgSrc} description={this.state.coffeeExpress.description}
-                githubLink={this.state.coffeeExpress.githubLink}/></li>
-                <li><GalleryItem title={this.state.coffeeExpress.title} imgSrc={this.state.coffeeExpress.imgSrc} description={this.state.coffeeExpress.description} githubLink={this.state.coffeeExpress.githubLink}/></li>
-                <li><GalleryItem title={this.state.coffeeExpress.title} imgSrc={this.state.coffeeExpress.imgSrc} description={this.state.coffeeExpress.description} githubLink={this.state.coffeeExpress.githubLink}/></li>
-                <li><GalleryItem title={this.state.coffeeExpress.title} imgSrc={this.state.coffeeExpress.imgSrc} description={this.state.coffeeExpress.description} githubLink={this.state.coffeeExpress.githubLink}/></li>
-                <li><GalleryItem title={this.state.coffeeExpress.title} imgSrc={this.state.coffeeExpress.imgSrc} description={this.state.coffeeExpress.description} githubLink={this.state.coffeeExpress.githubLink}/></li>
-                <li><GalleryItem title={this.state.coffeeExpress.title} imgSrc={this.state.coffeeExpress.imgSrc} description={this.state.coffeeExpress.description} githubLink={this.state.coffeeExpress.githubLink}/></li>
+                <li><GalleryItem {...coffeeExpress}/></li>
+                <li><GalleryItem {...coffeeExpress}/></li>
+                <li><GalleryItem {...coffeeExpress}/></li>
+                <li><GalleryItem {...coffeeExpress}/></li>
+                <li><GalleryItem {...coffeeExpress}/></li>
+                <li><GalleryItem {...coffeeExpress}/></li>
             </ul>
         </div>
     )}
