b86fafafc2388c1b1e1944e803d9a9aa53f23fe5
KamajiiSC <brandonlschaen@gmail.com>
2020-08-04 17:50:28 -0400

    Created ProjectList file in src with a list of project objects

---
+++ src/ProjectList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Projects = [
+  {
+    id: 0, 
+    title: 'Coffee Express', 
+    imgSrc: CoffeeExpress, 
+    description: 'A mock up website for a coffee shop', 
+    githubLink:'https://github.com/KamajiiSC/CoffeeExpress'
+  }
+]
+
+export default Projects;
+
