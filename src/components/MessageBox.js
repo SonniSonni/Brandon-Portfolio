48cc8d89f33ca0aabaf5387dad374667dec761eb
KamajiiSC <brandonlschaen@gmail.com>
2020-08-07 17:16:13 -0400

    Replaced flex container with a form

--- src/components/MessageBox.js
+++ src/components/MessageBox.js
@@ -3,12 +3,12 @@ import React from 'react';
 const MessageBox = () => (
   <div id="message-box">
     <h2>Message me right here!</h2>
-    <div id="message-flex-container">
+    <form>
       <input type='text'/>
       <input type='email'/>
       <textarea></textarea>
       <input className="button-style" type='submit' value='Send' />
-    </div>
+    </form>
   </div>
 );
 
