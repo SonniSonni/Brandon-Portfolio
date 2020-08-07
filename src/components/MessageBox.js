f93baee857bccf5eab45fb75c6a46989032eee2f
KamajiiSC <brandonlschaen@gmail.com>
2020-08-07 17:05:26 -0400

    Changed second input to text area

--- src/components/MessageBox.js
+++ src/components/MessageBox.js
@@ -5,7 +5,7 @@ const MessageBox = () => (
     <h2>Message me right here!</h2>
     <div id="message-flex-container">
       <input></input>
-      <input></input>
+      <textarea></textarea>
       <button>See more</button>
     </div>
   </div>
