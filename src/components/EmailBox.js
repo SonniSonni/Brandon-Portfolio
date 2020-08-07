86265ac762c3cc1f352de7da0668bafdcff3142e
KamajiiSC <brandonlschaen@gmail.com>
2020-08-07 18:02:57 -0400

    Removed unecessary img

--- src/components/EmailBox.js
+++ src/components/EmailBox.js
@@ -9,7 +9,7 @@ const EmailBox = () => (
     <p id="email-blurb">Always looking for new opportunities!</p>
     <div id="img-flex-container">
       <a target="_blank" href="https://github.com/KamajiiSC"><img src={GithubLogo}/></a>
-      <a><img src={LinkedLogo}/></a>
+      <a target="_blank" href="https://www.linkedin.com/in/brandon-schaen-53764216a/"><img src={LinkedLogo}/></a>
     </div>
   </div>
 );
