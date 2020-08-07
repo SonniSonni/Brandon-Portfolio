3ad7ca835f3b07d126539b5554ab7256960c2035
KamajiiSC <brandonlschaen@gmail.com>
2020-08-07 17:43:40 -0400

    Set id for each paragraph in email-box. Set text size for both pieces of text

--- src/components/EmailBox.js
+++ src/components/EmailBox.js
@@ -3,8 +3,8 @@ import React from 'react';
 const EmailBox = () => (
   <div id="email-box">
     <h2>Email Me</h2>
-    <h4>BrandonLSchaen@gmail.com</h4>
-    <p>Always looking for new opportunities!</p>
+    <p id="email-text">BrandonLSchaen@gmail.com</p>
+    <p id="email-blurb">Always looking for new opportunities!</p>
     <a><img /></a>
   </div>
 );
