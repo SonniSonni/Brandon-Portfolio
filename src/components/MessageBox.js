2e2e7a1eb6034892fe1ae20d6821996965b79992
KamajiiSC <brandonlschaen@gmail.com>
2020-08-07 17:34:14 -0400

    Set placeholder texts for input fields

--- src/components/MessageBox.js
+++ src/components/MessageBox.js
@@ -4,9 +4,9 @@ const MessageBox = () => (
   <div id="message-box">
     <h2>Message me right here!</h2>
     <form>
-      <input type='text'/>
-      <input type='email'/>
-      <textarea></textarea>
+      <input type='text' placeholder='Name'/>
+      <input type='email' placeholder='Email Address'/>
+      <textarea placeholder='Message' />
       <input className="button-style" type='submit' value='Send' />
     </form>
   </div>
