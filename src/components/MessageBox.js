621f70a03e73aac6e7c69fc0fb642f69b588b61d
KamajiiSC <brandonlschaen@gmail.com>
2020-08-07 16:51:12 -0400

    Changed message-box title

--- src/components/MessageBox.js
+++ src/components/MessageBox.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MessageBox = () => (
   <div>
-    <h2>Message Me</h2>
+    <h2>Message me right here!</h2>
     <input></input>
     <input></input>
     <button>See more</button>
