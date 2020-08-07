1eff5d58b1101a9241cdea4eeaed8dc75d22c2c3
KamajiiSC <brandonlschaen@gmail.com>
2020-08-07 17:55:25 -0400

    Wrapped EmailBox imgs in a flex-container

--- src/components/EmailBox.js
+++ src/components/EmailBox.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import GithubLogo from '../../resources/github-logo.png';
+import LinkedLogo from '../../resources/linkdin-logo.png';
 
 const EmailBox = () => (
   <div id="email-box">
     <h2>Email Me</h2>
     <p id="email-text">BrandonLSchaen@gmail.com</p>
     <p id="email-blurb">Always looking for new opportunities!</p>
-    <a><img src={GithubLogo}/></a>
-    <a><img /></a>
+    <div id="img-flex-container">
+      <a><img src={GithubLogo}/></a>
+      <a><img src={LinkedLogo}/></a>
+    </div>
   </div>
 );
 
