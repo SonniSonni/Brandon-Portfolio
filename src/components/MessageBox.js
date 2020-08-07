857d14526cde09e02e6521e3f9786fcdc044c5ba
KamajiiSC <brandonlschaen@gmail.com>
2020-08-07 16:57:24 -0400

    Wrapped message-box items in a flex container

--- src/components/MessageBox.js
+++ src/components/MessageBox.js
@@ -3,9 +3,11 @@ import React from 'react';
 const MessageBox = () => (
   <div id="message-box">
     <h2>Message me right here!</h2>
-    <input></input>
-    <input></input>
-    <button>See more</button>
+    <div id="message-flex-container">
+      <input></input>
+      <input></input>
+      <button>See more</button>
+    </div>
   </div>
 );
 
