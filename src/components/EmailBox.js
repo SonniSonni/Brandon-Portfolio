fe914d378b27a720c3cd7520d3e66b0923f32d03
KamajiiSC <brandonlschaen@gmail.com>
2020-08-07 17:51:24 -0400

    Inserted GithubLogo to EmailBox

--- src/components/EmailBox.js
+++ src/components/EmailBox.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import GithubLogo from '../../resources/github-logo.png';
 
 const EmailBox = () => (
   <div id="email-box">
     <h2>Email Me</h2>
     <p id="email-text">BrandonLSchaen@gmail.com</p>
     <p id="email-blurb">Always looking for new opportunities!</p>
+    <a><img src={GithubLogo}/></a>
     <a><img /></a>
   </div>
 );
