94ef2ec4a85bbd09f2a98838e6db745c03cf37ff
KamajiiSC <brandonlschaen@gmail.com>
2020-08-10 15:06:42 -0400

    Created Link for contact in footer and set color and decoration

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => (
     
     <footer>
         <p>© 2020 Brandon Schaen</p>
+        <Link to="/ContactPage" className="footer-link">Contact</Link>
     </footer>
 
 );
