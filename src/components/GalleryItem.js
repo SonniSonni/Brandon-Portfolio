f2b79057afd694605f32f4ee8b994ec6e7b885c3
KamajiiSC <brandonlschaen@gmail.com>
2020-08-11 17:10:10 -0400

    Wrapped gallery item img in img-preview

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -3,7 +3,9 @@ import Github from '../../resources/github-logo.png'
 
 const GalleryItem = (props) => (
       <div id="gallery-item">
-        <img src={props.imgSrc} alt="Preview of website" id="proj-prev"/>
+        <div className="img-preview">
+         <img src={props.imgSrc} alt="Preview of website" id="proj-prev"/>
+        </div>
         <div id='flex-container'>
           <h3> {props.title} </h3>
           <a target="_blank" href={props.githubLink}>
