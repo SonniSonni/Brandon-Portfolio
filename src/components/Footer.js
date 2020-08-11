c4070915b43d8a8204f37e776f4d66ff950ff0c4
KamajiiSC <brandonlschaen@gmail.com>
2020-08-11 12:24:18 -0400

    Changed footer classes to ids for now

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ const Footer = () => (
     
     <footer>
         <p>© 2020 Brandon Schaen</p>
-        <Link to="/ContactPage" className="footer-policy">Privacy Policy</Link>
-        <Link to="/ContactPage" className="footer-contact">Contact</Link>     
+        <Link to="/ContactPage" id="footer-policy">Privacy Policy</Link>
+        <Link to="/ContactPage" id="footer-contact">Contact</Link>     
     </footer>
 
 );
