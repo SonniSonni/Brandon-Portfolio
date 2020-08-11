93083540151ce54945c99884fd72b6ddcff745d2
KamajiiSC <brandonlschaen@gmail.com>
2020-08-11 14:36:01 -0400

    Added GithhubLogo LinkedinLogo and TwitterLogo into footer, then resized them to ml-size

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import GithubLogo from '../../resources/github-logo.png'
+import LinkedLogo from '../../resources/linkdin-logo.png';
+import TwitterLogo from '../../resources/twitter-logo.png';
 
 const Footer = () => (
     
     <footer>
         <p>© 2020 Brandon Schaen</p>
+        <div id="footer-socials">
+            <a target="_blank" href="https://github.com/KamajiiSC"><img src={GithubLogo}/></a>
+            <a target="_blank" href="https://www.linkedin.com/in/brandon-schaen-53764216a/"><img src={LinkedLogo}/></a>
+            <a target="_blank" href="#"><img src={TwitterLogo}/></a>
+        </div>
         <Link to="/ContactPage" id="footer-policy">Privacy Policy</Link>
         <Link to="/ContactPage" id="footer-contact">Contact</Link>     
     </footer>
