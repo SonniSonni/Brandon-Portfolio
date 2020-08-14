399d9dccdcbffade6514f75f390b386da0300852
KamajiiSC <brandonlschaen@gmail.com>
2020-08-14 17:23:29 -0400

    Removed padding from item description

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -5,7 +5,7 @@ const ProjectList = [
     id: 0, 
     title: 'Coffee Express', 
     imgSrc: CoffeeExpress, 
-    description: 'A simple example static info website for a coffee shop', 
+    description: 'A simple example static info website for a coffee shop.', 
     githubLink:'https://github.com/KamajiiSC/CoffeeExpress'
   }
 ]
