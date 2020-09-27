ed7acdf9cf7726d619c51c1c37b1bbd731df5b07
KamajiiSC <brandonlschaen@gmail.com>
2020-09-27 19:11:40 -0400

    Imported GalleryItem for porftfolio-container

--- src/Components/PortfolioPage/PortfolioPage.js
+++ src/Components/PortfolioPage/PortfolioPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './PortfolioPage.css';
+import GalleryItem from '../GalleryItem/GalleryItem';
 
 const PortfolioPage = () => (
   <div className="PortfolioPage">
     <div className="portfolio-container">.
-      
+      <GalleryItem />
     </div>
   </div>
 );
