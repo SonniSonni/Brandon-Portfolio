89e90469cebc8ca6e0c08ac92369e6f77e8ae5b7
KamajiiSC <brandonlschaen@gmail.com>
2020-09-27 19:32:53 -0400

    Changed HomePAge to home-page

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -4,7 +4,7 @@ import Hero from '../../Resources/hero.png';
 import GalleryPrev from '../GalleryPrev/GalleryPrev';
 
 const HomePage = () => (
-  <div className="HomePage">
+  <div className="home-page">
       <img src={Hero} alt="Man infront of city skyline" className="hero"/>
     <GalleryPrev />
   </div>
