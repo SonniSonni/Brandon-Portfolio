26e3f6c5bed9bb4d5a740701d3c88c7704d1e077
KamajiiSC <brandonlschaen@gmail.com>
2020-09-27 17:58:04 -0400

    Imported HomePage to the App

--- src/App/App.js
+++ src/App/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from '../logo.svg';
 import './App.css';
 import Header from '../Components/Header/Header';
+import HomePage from '../Components/HomePage/HomePage';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <HomePage />
     </div>
   );
 }
