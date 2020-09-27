e974002bfc3d0ab7eed0bfa0c3c3b45838413cef
KamajiiSC <brandonlschaen@gmail.com>
2020-09-27 18:03:07 -0400

    Imported BrowserRouter and Wrapped HomePage in a BrowserRouter

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from '../logo.svg';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header from '../Components/Header/Header';
 import HomePage from '../Components/HomePage/HomePage';
 
@@ -8,7 +9,9 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <HomePage />
+      <BrowserRouter>
+        <HomePage />
+      </BrowserRouter>
     </div>
   );
 }
