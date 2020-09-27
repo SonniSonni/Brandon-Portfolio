874fae0bd731c46a34595e97278b779627c894a1
KamajiiSC <brandonlschaen@gmail.com>
2020-09-27 18:35:16 -0400

    Imported GalleryItem to GalleryPrev

--- src/Components/GalleryPrev/GalleryPrev.js
+++ src/Components/GalleryPrev/GalleryPrev.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './GalleryPrev.css';
+import GalleryItem from '../GalleryItem/GalleryItem';
 
 const GalleryPrev = () => (
   <div className="GalleryPrev">
     <h1>Gallery Preview</h1>
     <hr />
       <div className="Gallery">
-        
+        <GalleryItem />
       </div>
     <hr />
   </div>
