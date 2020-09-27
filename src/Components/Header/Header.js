6b4f3ff5086b562f1a95b3175aa13111a3fed729
KamajiiSC <brandonlschaen@gmail.com>
2020-09-27 17:50:38 -0400

    Created nav and nav-list with 4 items

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,6 +5,14 @@ import headerImg from '../../Resources/headerImg.png';
 const Header = () => (
   <div className="header">
     <img src={headerImg} alt="Brandon Schaen" className="header-img"/>
+    <nav>
+      <ul className="nav-list">
+        <li>Home</li>
+        <li>Portfolio</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
   </div>
 );
 
