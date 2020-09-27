dac065e481823ee4ceffbe1586b61527fb9326e0
KamajiiSC <brandonlschaen@gmail.com>
2020-09-27 17:47:58 -0400

    Imported Header img and inserted it into Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './Header.css';
+import headerImg from '../../Resources/headerImg.png';
 
 const Header = () => (
   <div className="header">
-    
+    <img src={headerImg} alt="Brandon Schaen" className="header-img"/>
   </div>
 );
