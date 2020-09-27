beb57cfbfc5b3b485dfdcb7d40d06078429df895
KamajiiSC <brandonlschaen@gmail.com>
2020-09-27 19:14:28 -0400

    Created email and message divs

--- src/Components/ContactPage/ContactPage.js
+++ src/Components/ContactPage/ContactPage.js
@@ -4,7 +4,12 @@ import './ContactPage.css';
 const ContactPage = () => (
   <div className="ContactPage">
     <div className="contact-container">
-      
+      <div className="email">
+
+      </div>
+      <div className="message">
+        
+      </div>
     </div>
   </div>
 );
