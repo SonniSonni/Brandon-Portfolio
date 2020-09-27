5e9ea00b7d8b07e11d2ae21ce8473d155cbf6ea8
KamajiiSC <brandonlschaen@gmail.com>
2020-09-27 18:50:07 -0400

    Created express nodejs server with morgan and compression

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,10 +8,10 @@ const Header = () => (
     <img src={headerImg} alt="Brandon Schaen" className="header-img"/>
     <nav>
       <ul className="nav-list">
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/portfolio">Portfolio</NavLink></li>
-        <li><NavLink to="/about">About</NavLink></li>
-        <li><NavLink to="/contact">Contact</NavLink></li>
+        <li> <NavLink to="/">Home</NavLink> </li>
+        <li> <NavLink to="/portfolio">Portfolio</NavLink> </li>
+        <li> <NavLink to="/about">About</NavLink> </li>
+        <li> <NavLink to="/contact">Contact</NavLink> </li>
       </ul>
     </nav>
   </div>
