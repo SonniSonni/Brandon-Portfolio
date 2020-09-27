ece44adfc139389df26ecdd891eb6743e9d6fc87
KamajiiSC <brandonlschaen@gmail.com>
2020-09-27 18:27:54 -0400

    Added new resources, Created new img and imported the hero img

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './HomePage.css';
+import Hero from '../../Resources/hero.png';
 
 const HomePage = () => (
   <div className="HomePage">
-
+    <img src={Hero} alt="Man infront of city skyline" className="hero"/>
   </div>
 );
 
