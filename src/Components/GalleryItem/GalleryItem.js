fc7073ba3c9b7549617f2422fede9261cca9d774
KamajiiSC <brandonlschaen@gmail.com>
2020-09-30 17:40:15 -0400

    Inserted props into the gallery item

--- src/Components/GalleryItem/GalleryItem.js
+++ src/Components/GalleryItem/GalleryItem.js
@@ -3,11 +3,13 @@ import './GalleryItem.css';
 
 const GalleryItem = (props) => (
   <div className="gallery-item">
-    <img className="project-img" />
+    <img className="project-img" src={props.image} alt="Image of project"/>
     <div className="content">
-      <h3 className="title"></h3>
-      <a href="#" target="_blank" className="github-a"><img className="github-img" /></a>
-      <p className="description"></p>
+      <h3 className="title">{props.title}</h3>
+      <a href={props.github} target="_blank" className="github-a">
+        <img className="github-img" alt="Github logo" />
+      </a>
+      <p className="description">{props.description}</p>
     </div>
   </div>
 );
