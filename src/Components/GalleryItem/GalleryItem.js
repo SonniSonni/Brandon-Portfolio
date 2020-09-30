6bc352ba06a97ac2686f8b8ee78317078db71e2a
KamajiiSC <brandonlschaen@gmail.com>
2020-09-30 17:35:27 -0400

    Setup structure for a gallery item

--- src/Components/GalleryItem/GalleryItem.js
+++ src/Components/GalleryItem/GalleryItem.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './GalleryItem.css';
 
-const GalleryItem = () => (
+const GalleryItem = (props) => (
   <div className="gallery-item">
-
+    <img />
+    <div className="content">
+      <h3 className="title"></h3>
+      <a href="#" target="_blank"><img /></a>
+      <p className="description"></p>
+    </div>
   </div>
 );
 
