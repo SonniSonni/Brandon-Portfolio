58521269190f853bdf62a71427870808836fa1e1
KamajiiSC <brandonlschaen@gmail.com>
2020-09-30 20:05:17 -0400

    included github logos and inserted them into gallery items

--- src/Components/GalleryItem/GalleryItem.js
+++ src/Components/GalleryItem/GalleryItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './GalleryItem.css';
+import github from '../../Resources/GitHub-Mark-64px.png';
 
 const GalleryItem = (props) => (
   <div className="gallery-item">
@@ -10,7 +11,7 @@ const GalleryItem = (props) => (
     <div className="content">
       <h3 className="title">{props.title}</h3>
       <a href={props.github} target="_blank" className="github-a">
-        <img className="github-img" alt="Github logo" />
+        <img className="github-img" src={github}alt="Github logo" />
       </a>
       <p className="description">{props.description}</p>
     </div>
