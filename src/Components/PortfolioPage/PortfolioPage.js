5ea1fab41c42142bde95801d88eb3bac3afdd476
KamajiiSC <brandonlschaen@gmail.com>
2020-10-06 19:53:49 -0400

    Added 6 items, wrapped them in gallery-list, set grid display up with padding

--- src/Components/PortfolioPage/PortfolioPage.js
+++ src/Components/PortfolioPage/PortfolioPage.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './PortfolioPage.css';
 import GalleryItem from '../GalleryItem/GalleryItem';
+import List from '../../ProjectList'; 
 
 const PortfolioPage = () => (
   <div className="portfolio-page">
-    <div className="portfolio-container">.
-      <GalleryItem />
+    <div className="portfolio-container">
+      <div className="gallery-list">
+        <GalleryItem {...List[0]}/>
+        <GalleryItem {...List[0]}/>
+        <GalleryItem {...List[0]}/>
+        <GalleryItem {...List[0]}/>
+        <GalleryItem {...List[0]}/>
+        <GalleryItem {...List[0]}/>
+      </div>
     </div>
   </div>
 );
