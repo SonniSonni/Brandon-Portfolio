9a349ed920145b07aac0681a7f8e0d99dd8c8652
KamajiiSC <brandonlschaen@gmail.com>
2020-10-06 19:27:05 -0400

    Added containers in contact page

--- src/Components/ContactPage/ContactPage.js
+++ src/Components/ContactPage/ContactPage.js
@@ -5,10 +5,10 @@ const ContactPage = () => (
   <div className="contact-page">
     <div className="contact-container">
       <div className="email">
-
+        <h3>gg</h3>
       </div>
       <div className="message">
-        
+        <h3>gh</h3>
       </div>
     </div>
   </div>
