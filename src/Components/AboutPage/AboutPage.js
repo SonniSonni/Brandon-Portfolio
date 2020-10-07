14283615318bc25839bae4d828198f0aa7439371
KamajiiSC <brandonlschaen@gmail.com>
2020-10-06 20:00:56 -0400

    Setup divs and tags for about page

--- src/Components/AboutPage/AboutPage.js
+++ src/Components/AboutPage/AboutPage.js
@@ -5,6 +5,13 @@ const AboutPage = () => (
   <div className="about-page">
     <div className="about-container">
       <h1>About Me</h1>
+      <div className="content">
+        <p className="desc"></p>
+        <img src="" alt=""/>
+      </div>
+      <div className="resume">
+        
+      </div>
     </div>
   </div>
 );
