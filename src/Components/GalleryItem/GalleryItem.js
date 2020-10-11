51fe5667b0902700086252d21f6e81f52b4a73aa
KamajiiSC <brandonlschaen@gmail.com>
2020-10-11 19:48:08 -0400

    Added class project-display to anchor for img

--- src/Components/GalleryItem/GalleryItem.js
+++ src/Components/GalleryItem/GalleryItem.js
@@ -5,7 +5,7 @@ import github from '../../Resources/GitHub-Mark-64px.png';
 const GalleryItem = (props) => (
   <div className="gallery-item">
     <div className="image">
-      <a href={props.link} target="__blank">
+      <a className="project-display" href={props.link} target="__blank">
         <img className="project-img" src={props.image} alt="Project"/>
         <div className="overlay">
           <h3 className="overlay-title">{props.title}</h3>
