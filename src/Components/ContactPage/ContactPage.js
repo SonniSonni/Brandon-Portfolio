0bc2963d4e67f47bda6593282c08e83dbb4014f0
KamajiiSC <brandonlschaen@gmail.com>
2020-10-12 19:28:50 -0400

    Created sociallogos and sized and spaced them

--- src/Components/ContactPage/ContactPage.js
+++ src/Components/ContactPage/ContactPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './ContactPage.css';
+import github from '../../Resources/GitHub-Mark-64px.png';
+import link from '../../Resources/LI-In-Bug.png';
 
 const ContactPage = () => (
   <div className="contact-page">
@@ -10,6 +12,11 @@ const ContactPage = () => (
           <h2>Email Me!</h2>
           <h3>BrandonLSchaen@gmail.com</h3>
           <p>Always looking for new opportunities!</p>
+          <div className="contact-socials">
+            <a href="https://github.com/KamajiiSC" target="_blank" rel="noopener noreferrer"> <img src={github} alt="Github Logo"/> </a>
+            <a> </a>
+            <a href="https://www.linkedin.com/in/brandon-schaen/" target="_blank" rel="noopener noreferrer"> <img src={link} alt="Linked-In Logo"/> </a>
+          </div>
         </div>
         <div className="message">
           <h2>Message Me Here!</h2>
