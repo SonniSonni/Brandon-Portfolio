4e1abfca7ef466383a96796e5d4ee7e46a124c2d
KamajiiSC <brandonlschaen@gmail.com>
2020-10-12 17:42:50 -0400

    Set color on aboutPage

--- src/Components/AboutPage/AboutPage.js
+++ src/Components/AboutPage/AboutPage.js
@@ -6,7 +6,7 @@ const AboutPage = () => (
     <div className="about-container">
       <h1>About Me</h1>
       <div className="content">
-        <p className="desc"></p>
+        <p className="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tortor lorem, tempus vitae lorem at, lacinia cursus massa. Nulla facilisi. Vivamus accumsan mi non porttitor tempor. In egestas eu purus vel gravida. Aenean bibendum, sem nec auctor convallis, est ligula aliquam risus, in convallis ligula enim ut metus. Nam eu tellus rhoncus, eleifend risus vel, ullamcorper nunc. Phasellus nec turpis sit amet lacus accumsan vehicula. Quisque pharetra aliquam arcu nec imperdiet.</p>
         <img src="" alt=""/>
       </div>
       <div className="resume">
