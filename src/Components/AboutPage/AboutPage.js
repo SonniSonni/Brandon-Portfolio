087ea360faa8cfdec4f3205037c3a04ea0647a23
KamajiiSC <brandonlschaen@gmail.com>
2020-10-12 18:04:13 -0400

    Imported img, created a rounded image container and sized it correctly

--- src/Components/AboutPage/AboutPage.js
+++ src/Components/AboutPage/AboutPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './AboutPage.css';
+import Portrait from '../../Resources/profile.jpg';
 
 const AboutPage = () => (
   <div className="about-page">
@@ -7,7 +8,9 @@ const AboutPage = () => (
       <h1>About Me</h1>
       <div className="content">
         <p className="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tortor lorem, tempus vitae lorem at, lacinia cursus massa. Nulla facilisi. Vivamus accumsan mi non porttitor tempor. In egestas eu purus vel gravida. Aenean bibendum, sem nec auctor convallis, est ligula aliquam risus, in convallis ligula enim ut metus. Nam eu tellus rhoncus, eleifend risus vel, ullamcorper nunc. Phasellus nec turpis sit amet lacus accumsan vehicula. Quisque pharetra aliquam arcu nec imperdiet.</p>
-        <img src="" alt=""/>
+        <div className="portrait-container">
+          <img className="portrait" src={Portrait} alt=""/>
+        </div>
       </div>
       <div className="resume">
         
