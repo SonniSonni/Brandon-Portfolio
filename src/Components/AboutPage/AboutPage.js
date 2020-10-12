765872c148aea82c3d4cfdf9486a76d1bc4cb244
KamajiiSC <brandonlschaen@gmail.com>
2020-10-12 18:23:34 -0400

    Fixed name conflict

--- src/Components/AboutPage/AboutPage.js
+++ src/Components/AboutPage/AboutPage.js
@@ -7,7 +7,7 @@ const AboutPage = () => (
   <div className="about-page">
     <div className="about-container">
       <h1>About Me</h1>
-      <div className="content">
+      <div className="about-content">
         <p className="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tortor lorem, tempus vitae lorem at, lacinia cursus massa. Nulla facilisi. Vivamus accumsan mi non porttitor tempor. In egestas eu purus vel gravida. Aenean bibendum, sem nec auctor convallis, est ligula aliquam risus, in convallis ligula enim ut metus. Nam eu tellus rhoncus, eleifend risus vel, ullamcorper nunc. Phasellus nec turpis sit amet lacus accumsan vehicula. Quisque pharetra aliquam arcu nec imperdiet.</p>
         <div className="portrait-container">
           <img className="portrait" src={Portrait} alt=""/>
