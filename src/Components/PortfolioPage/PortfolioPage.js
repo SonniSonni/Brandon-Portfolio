6f34f5264b77327af2c3fa54f92b2f45a5dc4f2c
KamajiiSC <brandonlschaen@gmail.com>
2020-10-11 21:26:16 -0400

    Added title to portfolio and resized/moved gallery

--- src/Components/PortfolioPage/PortfolioPage.js
+++ src/Components/PortfolioPage/PortfolioPage.js
@@ -6,6 +6,7 @@ import List from '../../ProjectList';
 const PortfolioPage = () => (
   <div className="portfolio-page">
     <div className="portfolio-container">
+      <h1>My Work</h1>
       <div className="gallery-list">
         <GalleryItem {...List[0]}/>
         <GalleryItem {...List[1]}/>
