9c6ba9358938d79e669f82aa483131551f8b864f
KamajiiSC <brandonlschaen@gmail.com>
2020-10-12 22:34:08 -0400

    Passed driver to handle requests for server

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const http = require(http);
+const driver = require('./driver');
 
 const port = process.env.PORT || 3000;
 
-const server = http.createServer();
+const server = http.createServer(driver);
 
 server.listen(port);
