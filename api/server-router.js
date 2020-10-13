22ff6f43627c27c9444fa4672c00abece51778b3
KamajiiSC <brandonlschaen@gmail.com>
2020-10-13 16:22:47 -0400

    Set a successful post request to save the email and return the correct status and a message

--- api/server-router.js
+++ api/server-router.js
@@ -5,9 +5,23 @@ const Email = require('./email-model');
 
 router.post('/contact?name=&email-add=&message=', (req, res, next) => {
   const email = new Email({
-  _id: new mongoose.Types.ObjectId(),
-  name: req.body.name,
-  email: req.body.email,
-  message: req.body.message
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    email: req.body.email,
+    message: req.body.message
   });
+  email.save()
+    .then(result => {
+      console.log(result);
+      res.statuis(201).json({
+        message: 'Email Sent',
+        sentEmail: {
+          name: result.name,
+          email: result.email,
+          message: result.message,
+          _id: result._id
+        }
+      })
+    }) 
+    .catch()
 })
