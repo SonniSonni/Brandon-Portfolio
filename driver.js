dfc603ff2e04b50801cb4d83a50d502c05d111ab
KamajiiSC <brandonlschaen@gmail.com>
2020-10-12 22:43:01 -0400

    Setup morgan and bodyParser

--- driver.js
+++ driver.js
@@ -3,3 +3,7 @@ const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
+app.use(morgan('dev'));
+app.unsubscribe(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
