ecbe287bf37d10dd99f0c3badfd362754ec220b0
KamajiiSC <brandonlschaen@gmail.com>
2020-10-13 16:19:22 -0400

    Setup post request to create a new email object

--- api/server-router.js
+++ api/server-router.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const Email = require('./email-model');
+
+router.post('/contact?name=&email-add=&message=', (req, res, next) => {
+  const email = new Email({
+  _id: new mongoose.Types.ObjectId(),
+  name: req.body.name,
+  email: req.body.email,
+  message: req.body.message
+  });
+})
