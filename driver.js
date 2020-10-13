ef7e23ff970eb6fb4f2aece755e632d8864f71a4
KamajiiSC <brandonlschaen@gmail.com>
2020-10-13 18:09:50 -0400

    Setup additional error handling and exported app

--- driver.js
+++ driver.js
@@ -27,3 +27,19 @@ app.use((req, res, next) => {
 
 app.use('/contact', emailRouter);
 
+app.use((req, res, next) => {
+  const error = new Error('NOT FOUND');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  })
+});
+
+module.exports = app;
