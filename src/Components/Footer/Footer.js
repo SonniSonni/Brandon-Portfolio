baa750241c4df8f35019e03c1c1a058ecd2cb324
KamajiiSC <brandonlschaen@gmail.com>
2020-10-12 22:17:54 -0400

    Layed out footer content

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,13 @@ import './Footer.css';
 
 const Footer = () => (
   <div className="footer-wrap">
-    <h4>Brandon</h4>
+    <ul className="footer-nav">
+      <li><h4>Terms of Use</h4></li>
+      <li><h4>Privacy Policy</h4></li>
+      <li><h4>Contact</h4></li>
+      <li><h4>Sitemap</h4></li>
+      <li><h4>2020 ©</h4></li>
+    </ul>
   </div>
 );
 
