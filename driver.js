2911223ea405e2d0c8338d045baf9c039356c95d
KamajiiSC <brandonlschaen@gmail.com>
2020-10-12 22:49:14 -0400

    Set up all the headers to be passed

--- driver.js
+++ driver.js
@@ -7,3 +7,13 @@ const mongoose = require('mongoose');
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header('Acces-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  if(req.method === "OPTIONS"){
+    res.header('Access-Control-Allow-Methods', 'POST')
+    return res.status(200).json({});
+  }
+  next();
+}); 
