331bde86ab088fdd4cc07da549f64b5009f9591a
KamajiiSC <brandonlschaen@gmail.com>
2020-10-13 16:12:00 -0400

    Setup schema for mcontact page message

---
+++ api/email-model.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const emailSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  name: {type: String, required: true},
+  email: {type: String, required: true},
+  message: {type: String, required: true}
+})
