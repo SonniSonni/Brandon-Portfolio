52311ba76d351fe038ad4a64512f1c37b1047790
KamajiiSC <brandonlschaen@gmail.com>
2020-10-22 16:13:39 -0400

    Made fetch request check if response includes ok flag, if not throw an error and added a catch block for logging the error

--- client/src/Components/ContactPage/ContactPage.js
+++ client/src/Components/ContactPage/ContactPage.js
@@ -19,8 +19,18 @@ const formSubmit = (e) =>{
       "Content-type": "application/json; charset=UTF-8"
     }
   })
-  .then(response => response.json())
-  .then(json => console.log(json));
+  .then((response) => {
+    if(response.ok){
+      response.json();
+    } else {
+      throw new Error('Something went wrong');
+    }
+  })
+  .then(json => console.log(json))
+  .catch((error) => {
+    console.log(error);
+  });
+  
 
   e.preventDefault();
 }
