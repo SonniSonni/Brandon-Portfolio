02a9aae4106e921c6d5d8eb1327fcb713baac96c
KamajiiSC <brandonlschaen@gmail.com>
2020-11-02 18:41:00 -0500

    Wrapped Terms of use in navlink

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => (
   <div className="footer-wrap">
     <ul className="footer-nav">
-      <li><h4>Terms of Use</h4></li>
+      <li><NavLink exact to="/terms" className="footer-link" activeClassName="active"><h4>Terms of Use</h4></NavLink></li>
       <li><h4>Privacy Policy</h4></li>
       <li><h4>Contact</h4></li>
       <li><h4>Sitemap</h4></li>
