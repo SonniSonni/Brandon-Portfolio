688446e0954e26dc4610ce0b6190107140f1e3f5
KamajiiSC <brandonlschaen@gmail.com>
2020-11-02 20:39:59 -0500

    Removed google from advertising section

--- client/src/Components/PrivacyPage/PrivacyPage.js
+++ client/src/Components/PrivacyPage/PrivacyPage.js
@@ -18,21 +18,10 @@ const PrivacyPage = () => (
     <p>Like any other website, BrandonSchaen.com uses 'cookies'. These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.</p>
 
     <p>For more general information on cookies, please read <a href="https://www.privacypolicies.com/blog/cookies/">"What Are Cookies"</a>.</p>
-
-    <h2>Google DoubleClick DART Cookie</h2>
-
-    <p>Google is one of a third-party vendor on our site. It also uses cookies, known as DART cookies, to serve ads to our site visitors based upon their visit to www.BrandonSchaen.com and other sites on the internet. However, visitors may choose to decline the use of DART cookies by visiting the Google ad and content network Privacy Policy at the following URL – <a href="https://policies.google.com/technologies/ads">https://policies.google.com/technologies/ads</a></p>
-
+    
     <h2>Our Advertising Partners</h2>
 
     <p>Some of advertisers on our site may use cookies and web beacons. Our advertising partners are listed below. Each of our advertising partners has their own Privacy Policy for their policies on user data. For easier access, we hyperlinked to their Privacy Policies below.</p>
-
-    <ul>
-        <li>
-            <p>Google</p>
-            <p><a href="https://policies.google.com/technologies/ads">https://policies.google.com/technologies/ads</a></p>
-        </li>
-    </ul>
 
     <h2>Privacy Policies</h2>
 
