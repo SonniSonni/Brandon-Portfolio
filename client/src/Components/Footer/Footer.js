bacdea1dc291d84801bdf0260656c0d095966a7c
KamajiiSC <brandonlschaen@gmail.com>
2020-11-02 20:05:21 -0500

    Fixed navlink import

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Footer.css';
-import NavLink from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Footer = () => (
   <div className="footer-wrap">
