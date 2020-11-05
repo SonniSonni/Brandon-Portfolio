07c7b283dc720946df5487af6cae76f61c4a80e3
KamajiiSC <brandonlschaen@gmail.com>
2020-11-05 05:38:14 -0500

    Set menu items to hide menu

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -17,10 +17,48 @@ export default class Header extends Component{
     <img src={headerImg} alt="Brandon Schaen" className="header-img"/>
     <nav  className={this.state.isHam ? "" : "hidden" }>
       <ul className="nav-list">
-        <li> <NavLink exact to="/" className="nav-link" activeClassName="active">Home</NavLink> </li>
-        <li> <NavLink to="/portfolio" className="nav-link" activeClassName="active">Portfolio</NavLink> </li>
-        <li> <NavLink to="/about" className="nav-link" activeClassName="active">About</NavLink> </li>
-        <li> <NavLink to="/contact" className="nav-link contact" activeClassName="active">Contact</NavLink> </li>
+        <li> <NavLink onClick={() => {
+          if(this.state.isHam === "hidden"){
+            this.setState({isHam:""})  
+          }
+          else{
+            this.setState({isHam:"hidden"})  
+          }
+        }}
+        exact to="/" className="nav-link" 
+        activeClassName="active">Home</NavLink> </li>
+
+        <li> <NavLink onClick={() => {
+          if(this.state.isHam === "hidden"){
+            this.setState({isHam:""})  
+          }
+          else{
+            this.setState({isHam:"hidden"})  
+          }
+        }}
+        to="/portfolio" 
+        className="nav-link" activeClassName="active">Portfolio</NavLink> </li>
+
+        <li> <NavLink onClick={() => {
+          if(this.state.isHam === "hidden"){
+            this.setState({isHam:""})  
+          }
+          else{
+            this.setState({isHam:"hidden"})  
+          }
+          }}
+          to="/about" 
+          className="nav-link" activeClassName="active">About</NavLink> </li>
+        <li> <NavLink onClick={() => {
+          if(this.state.isHam === "hidden"){
+            this.setState({isHam:""})  
+          }
+          else{
+            this.setState({isHam:"hidden"})  
+          }
+        }}
+        to="/contact" 
+        className="nav-link contact" activeClassName="active">Contact</NavLink> </li>
       </ul>
     </nav>
     <button onClick={() => {
