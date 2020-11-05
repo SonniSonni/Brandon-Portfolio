e6a8c6cab5751e57081987d259fb47cc1af864f9
KamajiiSC <brandonlschaen@gmail.com>
2020-11-05 05:56:15 -0500

    Imported React-modal and setup state to handle opening and closing modal

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -3,13 +3,25 @@ import './Header.css';
 import headerImg from '../../Resources/headerImg.png';
 import ham from '../../Resources/ham.png';
 import { NavLink } from 'react-router-dom';
+import Modal from 'react-modal';
 
 export default class Header extends Component{
   constructor(props) {
     super(props);
     this.state = {
-      isHam: 'hidden'
+      showModal: false
     };
+
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+
+  handleOpenModal () {
+    this.setState({showModal: true});
+  }
+
+  handleCloseModal () {
+    this.setState({showModal: false});
   }
   render(){
     return(
