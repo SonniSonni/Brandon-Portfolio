d42a2022bcb7e22037f3dc316224e91637fe7850
KamajiiSC <brandonlschaen@gmail.com>
2020-11-05 05:19:51 -0500

    Setup hamburger toggle

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Header.css';
 import headerImg from '../../Resources/headerImg.png';
 import ham from '../../Resources/ham.png';
 import { NavLink } from 'react-router-dom';
 
-const Header = () => (
+export default class Header extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHam: 'hidden'
+    };
+  }
+  render(){
+    return(
   <div className="header">
     <img src={headerImg} alt="Brandon Schaen" className="header-img"/>
-    <nav>
+    <nav  className={this.state.isHam ? "" : "hidden" }>
       <ul className="nav-list">
         <li> <NavLink exact to="/" className="nav-link" activeClassName="active">Home</NavLink> </li>
         <li> <NavLink to="/portfolio" className="nav-link" activeClassName="active">Portfolio</NavLink> </li>
@@ -15,10 +23,18 @@ const Header = () => (
         <li> <NavLink to="/contact" className="nav-link contact" activeClassName="active">Contact</NavLink> </li>
       </ul>
     </nav>
-    <button className="ham-btn" aria-expanded="false" aria-controls="menu"> 
+    <button onClick={() => {
+      if(this.state.isHam === "hidden"){
+        this.setState({isHam:""})  
+      }
+      else{
+        this.setState({isHam:"hidden"})  
+      }
+    }} 
+      className={'ham-btn'} aria-expanded="false" aria-controls="menu"> 
       <img src={ham} alt="menu" className="ham" />
     </button> 
   </div>
-);
-
-export default Header;
+  )
+  }
+};
