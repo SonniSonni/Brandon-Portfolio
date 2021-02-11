b0fbc2acee37341a9eb186d7d16e9c1e6348b36c
KamajiiSC <brandonlschaen@gmail.com>
2021-02-11 14:41:33 -0500

    Wrapped Matters into a span for style. Set LandingPage color to white and set Matters to our sites orange

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -7,7 +7,7 @@ const LandingPage = () => (
   <div className="landing">
     <Header />
     <img src="" alt="Brandon Schaen logo" className="landing-logo"/>
-    <h1 className="landing-title">Your internet presence Matters</h1>
+    <h1 className="landing-title">Your internet presence <span className="matters">Matters</span></h1>
     <h2 className="landing-desc">Invest in someone who will treat it like it does. 
 My mission is to always deliver quality results that will overshadow the rest. </h2>
     <LandingBtn />
