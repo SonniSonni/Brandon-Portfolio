a3315ab25fddd269836f66668adbc218b088654f
KamajiiSC <brandonlschaen@gmail.com>
2021-02-11 18:00:07 -0500

    Added stripe svg. Added new font, redid some styling and sizes

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -3,19 +3,20 @@ import './LandingPage.css';
 import Header from '../Header/Header';
 import LandingBtn from '../LandingBtn/LandingBtn';
 import Logo from '../../resources/brandon-logo.svg';
+import LandingStripe from '../../resources/landing-stripe.svg';
 
 const LandingPage = () => (
   <div className="landing">
     <Header />
     <div className="logo-title">
       <img src={Logo} alt="Brandon Schaen logo" className="landing-logo"/>
-      <h1 className="landing-title">Your internet presence <span className="matters">Matters</span></h1>
+      <h1 className="landing-title">Your internet presence <br /><span className="matters">Matters</span></h1>
     </div>
     <div className="desc-btn">
       <h2 className="landing-desc">Invest in someone who will treat it like it does. My mission is to always deliver quality results that will overshadow the rest. </h2>
       <LandingBtn />
     </div>
-    
+    <img src={LandingStripe} alt="landing-stripe" className="landing-stripe"/>
   </div>
 );
 
