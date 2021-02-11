fbd2d58aebd24922da7b86c5233f2fabd24a269e
KamajiiSC <brandonlschaen@gmail.com>
2021-02-11 14:44:52 -0500

    Wrapped elements into two different container divs

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -6,11 +6,15 @@ import LandingBtn from '../LandingBtn/LandingBtn';
 const LandingPage = () => (
   <div className="landing">
     <Header />
-    <img src="" alt="Brandon Schaen logo" className="landing-logo"/>
-    <h1 className="landing-title">Your internet presence <span className="matters">Matters</span></h1>
-    <h2 className="landing-desc">Invest in someone who will treat it like it does. 
-My mission is to always deliver quality results that will overshadow the rest. </h2>
-    <LandingBtn />
+    <div className="logo-title">
+      <img src="" alt="Brandon Schaen logo" className="landing-logo"/>
+      <h1 className="landing-title">Your internet presence <span className="matters">Matters</span></h1>
+    </div>
+    <div className="desc-btn">
+      <h2 className="landing-desc">Invest in someone who will treat it like it does. My mission is to always deliver quality results that will overshadow the rest. </h2>
+      <LandingBtn />
+    </div>
+    
   </div>
 );
 
