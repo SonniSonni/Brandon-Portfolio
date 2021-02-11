9430ef9e4ca8ee94993c622859d5a4a31032721d
KamajiiSC <brandonlschaen@gmail.com>
2021-02-11 14:37:02 -0500

    Created LandingBtn component, Added elements to LandingPage

---
+++ client/src/components/LandingBtn/LandingBtn.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LandingBtn = () => (
+  <div>
+    Button
+  </div>
+);
+
+export default LandingBtn;
--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './LandingPage.css';
 import Header from '../Header/Header';
+import LandingBtn from '../LandingBtn/LandingBtn';
 
 const LandingPage = () => (
   <div className="landing">
     <Header />
+    <img src="" alt="Brandon Schaen logo" className="landing-logo"/>
+    <h1 className="landing-title"></h1>
+    <h2 className="landing-desc"></h2>
+    <LandingBtn />
   </div>
 );
 
