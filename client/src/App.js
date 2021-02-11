65f87ac02ce8acb69a44842798ca2d98e1c8cfce
KamajiiSC <brandonlschaen@gmail.com>
2021-02-11 13:34:27 -0500

    Created Header component and imported to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <ul>
+      <li>Home</li>
+      <li>Work</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </header>
+);
+
+export default Header;
