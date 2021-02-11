a26f0f0edc3332cab61232c44815c503e8d1258b
KamajiiSC <brandonlschaen@gmail.com>
2021-02-11 14:05:39 -0500

    Added Header to LandingPage, imported LandingPage to App and removed Header from App

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Header from './components/Header/Header';
+import LandingPage from './components/LandingPage/LandingPage';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <LandingPage />
     </div>
   );
 }
--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Header from '../Header/Header';
 
 const LandingPage = () => (
   <div>
-    
+    <Header />
   </div>
 );
+
+export default LandingPage;
