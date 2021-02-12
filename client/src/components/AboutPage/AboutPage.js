212532501cad18edf18b0b67cfcd104d645aca26
KamajiiSC <brandonlschaen@gmail.com>
2021-02-12 16:57:41 -0500

    Wrapped p tags in a section and centered it

--- client/src/components/AboutPage/AboutPage.js
+++ client/src/components/AboutPage/AboutPage.js
@@ -6,12 +6,20 @@ const AboutPage = () => (
   <section id="about" className="about-page">
     <h2 className="about-title">Me</h2>
     <section className="desc-img">
-      <p className="personal">Just a front-end web developer from 
-Baltimore, Maryland. I wrote my first lines of code when I was only 12, but have spent the better part of the last 4 years learning everything I can about the world of the web.</p>
-      <img src={face} alt=""/>
+      <section className="desc">
+        <p className="personal">Just a front-end web developer from 
+        <br />
+        Baltimore, Maryland.
+        </p>
+        <p> I wrote my first lines of code when I was only 12, but have spent the better part of the last 4 years learning everything I can about the world of the web.</p>
+      </section>
+      
+      <img src={face} alt="Self Portrait"/>
     </section>
     
-    <section className="skills">skills</section>
+    <section className="skills">
+      skills
+    </section>
   </section>
 );
 
