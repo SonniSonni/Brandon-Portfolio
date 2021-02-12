8dafc8a711b0a09c6b2ee37b084a12092bb5bfe7
KamajiiSC <brandonlschaen@gmail.com>
2021-02-12 17:02:24 -0500

    Resized desc and sized personal  font size

--- client/src/components/AboutPage/AboutPage.js
+++ client/src/components/AboutPage/AboutPage.js
@@ -11,7 +11,7 @@ const AboutPage = () => (
         <br />
         Baltimore, Maryland.
         </p>
-        <p> I wrote my first lines of code when I was only 12, but have spent the better part of the last 4 years learning everything I can about the world of the web.</p>
+        <p className="personal"> I wrote my first lines of code when I was only 12, but have spent the better part of the last 4 years learning everything I can about the world of the web.</p>
       </section>
       
       <img src={face} alt="Self Portrait"/>
