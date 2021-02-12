4f8d042537092d7904a5f4e969da9d65ca3b5a24
KamajiiSC <brandonlschaen@gmail.com>
2021-02-12 16:48:29 -0500

    Added portrait and para text

--- client/src/components/AboutPage/AboutPage.js
+++ client/src/components/AboutPage/AboutPage.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './AboutPage.css';
+import face from "../../resources/my-face.jpg";
 
 const AboutPage = () => (
   <section id="about" className="about-page">
-    <h2 className="about-title">About</h2>
+    <h2 className="about-title">Me</h2>
+    <section className="desc-img">
+      <p className="personal">Just a front-end web developer from 
+Baltimore, Maryland. I wrote my first lines of code when I was only 12, but have spent the better part of the last 4 years learning everything I can about the world of the web.</p>
+      <img src={face} alt=""/>
+    </section>
   </section>
 );
 
