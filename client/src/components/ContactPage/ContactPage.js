270e86a7590c6d8ffde29feb1d492e3f820069cb
KamajiiSC <brandonlschaen@gmail.com>
2021-02-15 14:54:59 -0500

    Spacing sizing and added a twitter icon

--- client/src/components/ContactPage/ContactPage.js
+++ client/src/components/ContactPage/ContactPage.js
@@ -2,21 +2,22 @@ import React from 'react';
 import './ContactPage.css';
 import git from '../../resources/git-ico.png';
 import linkedin from '../../resources/linked-logo.png';
+import twitter from '../../resources/twitter-icon.png';
 
 const ContactPage = () => (
   <section id="contact" className="contact-page">
     <h2 className="contact-title">Contact</h2>
     <section className="form-container">
       <form className="contact-form" action="">
-        <div>
+        <div className="input-container">
           <label htmlFor="name-input">Name</label>
           <input type="text" name="name-input" id="name-input"/>
         </div>
-        <div>
+        <div className="input-container">
           <label htmlFor="email-input">Email</label>
           <input type="email" name="email-input" id="email-input"/>
         </div>
-        <div>
+        <div className="input-container">
           <label htmlFor="message-input">Message</label>
           <textarea name="message-input" id="message-input" cols="30" rows="10"></textarea>
         </div>
@@ -24,6 +25,7 @@ const ContactPage = () => (
           <section className="socials">
             <a rel="noopener noreferrer" href="https://github.com/KamajiiSC" target="_blank"><img src={git} alt=""/></a>  
             <a href="https://www.linkedin.com/in/brandon-schaen/" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt=""/></a>
+            <a href="https://twitter.com/KamajiiJeesh" target="_blank" rel="noopener noreferrer"><img src={twitter} alt=""/></a>
           </section>
           
           <button id="submit-btn" type="button">SAY HI</button>
