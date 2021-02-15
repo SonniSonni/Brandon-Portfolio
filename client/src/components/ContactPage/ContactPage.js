86368907384496c37a9abefea41257528e555f0d
KamajiiSC <brandonlschaen@gmail.com>
2021-02-15 14:15:47 -0500

    Setup basic form-container and form, Sized form-container and created border

--- client/src/components/ContactPage/ContactPage.js
+++ client/src/components/ContactPage/ContactPage.js
@@ -4,6 +4,11 @@ import './ContactPage.css';
 const ContactPage = () => (
   <section id="contact" className="contact-page">
     <h2 className="contact-title">Contact</h2>
+    <section className="form-container">
+      <form action="">
+        
+      </form>
+    </section>
   </section>
 );
 
