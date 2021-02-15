e6859bb6b09c1770ce4fec625aa0a86f6ebe907a
KamajiiSC <brandonlschaen@gmail.com>
2021-02-15 14:42:23 -0500

    Spaced and sized form. Wrapped form elements in divs

--- client/src/components/ContactPage/ContactPage.js
+++ client/src/components/ContactPage/ContactPage.js
@@ -8,15 +8,18 @@ const ContactPage = () => (
     <h2 className="contact-title">Contact</h2>
     <section className="form-container">
       <form className="contact-form" action="">
-        <label htmlFor="name-input">Name</label>
-        <input type="text" name="name-input" id="name-input"/>
-
-        <label htmlFor="email-input">Email</label>
-        <input type="email" name="email-input" id="email-input"/>
-
-        <label htmlFor="message-input">Message</label>
-        <textarea name="message-input" id="message-input" cols="30" rows="10"></textarea>
-
+        <div>
+          <label htmlFor="name-input">Name</label>
+          <input type="text" name="name-input" id="name-input"/>
+        </div>
+        <div>
+          <label htmlFor="email-input">Email</label>
+          <input type="email" name="email-input" id="email-input"/>
+        </div>
+        <div>
+          <label htmlFor="message-input">Message</label>
+          <textarea name="message-input" id="message-input" cols="30" rows="10"></textarea>
+        </div>
         <section className="social-submit">
           <section className="socials">
             <a rel="noopener noreferrer" href="https://github.com/KamajiiSC" target="_blank"><img src={git} alt=""/></a>  
