6f40b5354b7e534fbd69939a1ff60f28c48e6b2e
KamajiiSC <brandonlschaen@gmail.com>
2021-02-15 17:14:19 -0500

    Added structure for card, rendered 2 cards in a new container on projects

--- client/src/components/ProjectCard/ProjectCard.js
+++ client/src/components/ProjectCard/ProjectCard.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './ProjectCard.css';
+import greg from '../../resources/grg.PNG';
 
-const ProjectCard = () => (
+const ProjectCard = (props) => (
   <div className="card-container">
+    <img src={greg} alt=""/>
+    <div className="card-desc">
+      <h3 className="card-title"></h3>
+      <p className="card-description"></p>
+      <ul className="tech-list">
 
+      </ul>
+      <div className="icons"></div>
+    </div>
   </div>
 );
 
--- client/src/components/ProjectsPage/ProjectsPage.js
+++ client/src/components/ProjectsPage/ProjectsPage.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './ProjectsPage.css';
+import ProjectCard from '../ProjectCard/ProjectCard';
 
 const ProjectsPage = () => (
   <section id="projects" className="projects-page">
     <h2 className="projects-title">Projects</h2>
+    <section className="projects-container">
+      <ProjectCard /> 
+      <ProjectCard /> 
+    </section>
   </section>
 );
 
