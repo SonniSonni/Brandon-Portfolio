d52a6cf1d120da4e34eddde310081a010bb7ee6f
KamajiiSC <brandonlschaen@gmail.com>
2021-02-15 19:17:31 -0500

    Setup grid for display and made carousel cards, dot nav

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => (
     <nav>
       <ul>
         <li>
-          <Link to="home" activeClass="active" spy={true} smooth={true}>Home</Link>
+          <Link to="last" activeClass="active" spy={true} smooth={true}>Home</Link>
         </li>
         <li>
           <Link to="projects" smooth={true}>Work</Link>
--- client/src/components/ProjectsPage/ProjectsPage.js
+++ client/src/components/ProjectsPage/ProjectsPage.js
@@ -2,13 +2,29 @@ import React from 'react';
 import './ProjectsPage.css';
 import ProjectCard from '../ProjectCard/ProjectCard';
 
+const slide = () => {
+  const container = document.querySelector(".projects-container");
+  container.style.left = container.style.left + 10;
+};
+
 const ProjectsPage = () => (
   <section id="projects" className="projects-page">
-    <h2 className="projects-title">Projects</h2>
+    <h2 className="projects-title full">Projects</h2>
     <section className="projects-container">
-      <ProjectCard /> 
-      <ProjectCard /> 
-      <ProjectCard /> 
+      <div id="car1" className="car show">
+        <ProjectCard /> 
+        <ProjectCard /> 
+        <ProjectCard />
+      </div>
+      <div id="car2" className="car hide">
+        <ProjectCard /> 
+        <ProjectCard /> 
+        <ProjectCard /> 
+      </div>
+      <ul className="car-menu">
+        <li><span className="dot active-dot"></span></li>
+        <li><span className="dot"></span></li>
+      </ul>
     </section>
   </section>
 );
