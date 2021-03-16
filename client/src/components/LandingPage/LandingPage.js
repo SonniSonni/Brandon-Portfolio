a0f837e457712f5acc5b9872f44d7e4678c0f5dd
KamajiiSC <brandonlschaen@gmail.com>
2021-03-16 16:57:06 -0400

    Restructured landing page to make it responsive

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -15,11 +15,15 @@ const LandingPage = () => {
       {/* <StyledLogo className="landing-logo"/> */}
       <h1 className="landing-title">Your internet presence <br /><span className="matters">Matters</span></h1>
     </div>
-    <div className="desc-btn">
-      <h2 className="landing-desc">Invest in someone who will treat it like it does. <br />My mission is to always deliver quality results that will overshadow the rest. </h2>
-      <LandingBtn />
-    </div>
-    <img src={LandingStripe} alt="landing-stripe" className="landing-stripe"/>
+
+    <div className="bottom-landing">
+      <img src={LandingStripe} alt="landing-stripe" className="landing-stripe"/>
+      <div className="desc-btn">  
+        <h2 className="landing-desc">Invest in someone who will treat it like it does. <br />My mission is to always deliver quality results that will overshadow the rest. </h2>
+        <LandingBtn />
+      </div>
+    </div>   
+    
   </div>
   )
 };
